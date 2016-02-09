7ed365d6494418db800c0cc5ee831785d64f4a31
Nicolas Sarlin <nico.sarlin@gmail.com>
2016-02-09 22:48:23 +0100

    vectorized cost function for NN

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,6 +1,27 @@
 import numpy as np
+import numbers
 from Model import Model
 from Utils import *
+
+def label_to_bitvector(lbl, nb_classes):
+    """
+    Converts a lbl to a bitvector where the lblth element is 1 and the others 0
+    """
+    if isinstance(lbl, numbers.Real):
+        return np.array([1 if i == lbl else 0 for i in range(nb_classes)])
+    else:
+        raise TypeError
+
+def labels_to_bitmatrix(lbls, nb_classes):
+    """
+    Converts a vector of labels to a matrix made of bitvectors.
+    """
+    if isinstance(lbls, np.ndarray) and \
+       (len(lbls.shape) == 1 or lbls.shape[1] == 1):
+        return np.array([label_to_bitvector(lbl, nb_classes) for lbl in lbls])
+    else:
+        raise TypeError
+
 
 class NeuralNetwork(Model):
     """
@@ -110,6 +131,24 @@ class NeuralNetwork(Model):
             acc_r = 0
             for l in range(len(coefs)):
                 acc_r += np.sum(np.square(coefs[l][:, 1:]))
-            return acc/m + self.regul/(2*m) * acc_r
-        else:
-            return acc/m
+            acc += self.regul/2 * acc_r
+
+        return acc/m
+
+
+    def cost_vect(self, coefs, X, y):
+        """
+        Vectorized implementation of cost function.
+        """
+        m = X.shape[0]
+
+        Y = labels_to_bitmatrix(y, self.shape[-1])
+        probs = self.hypothesis(coefs, X)
+        ucost = np.sum(np.sum(np.multiply(-Y, np.log(probs)) -\
+                              np.multiply(1-Y, np.log(1-probs))))
+        if self.regul != 0:
+            for mat in coefs:
+                ucost += self.regul/2 * np.sum(np.square(mat[:, 1:]))
+
+
+        return ucost/m
